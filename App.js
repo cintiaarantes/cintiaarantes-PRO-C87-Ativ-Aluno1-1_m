5fbbbb181ba0c09fb8bc475f5adc51faf068e3f6
Cíntia Arantes <114604292+cintiaarantes@users.noreply.github.com>
2023-10-17 16:04:37 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+//Desafio 01: Importar as telas criadas 
+
+
+//Desafio 02: Criar uma função createStackNavigator e armazenar em uma constante chamada Stack
+
+
+export default function App() {
+  return (
+    //Desafio 03: Usar o componente NavigationContainer e dentro dele adicionar as telas. 
+
+  );
+}
